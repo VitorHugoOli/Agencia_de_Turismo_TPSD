1fa08a674a7cbca7bf4db56cb3eca14ee143da05
Vitor Hugo <vho2009@hotmail.com>
2020-10-30 19:10:23 -0300

    Resolvendo conflito gerado pelo jose 2.0

--- hospServer.py
+++ hospServer.py
@@ -1,30 +1,33 @@
 import asyncio
 import datetime
 import json
+
 from sockets.server import EasySocketServer
 
 
 def getHosps(hosps, ida, cidade, pessoas):
     rListHosp = []
     for h in hosps:
-        if(h['cidade'] == cidade):
-            if((datetime.datetime.strptime(h['diaEntrada'], "%d/%m/%Y") == datetime.datetime.strptime(ida,'%Y-%m-%d')) & (h['qtdCamas'] >= pessoas)):
+        if (h['cidade'] == cidade):
+            if ((datetime.datetime.strptime(h['diaEntrada'], "%d/%m/%Y") == datetime.datetime.strptime(ida,
+                                                                                                       '%Y-%m-%d')) & (
+                    h['qtdCamas'] >= pessoas)):
                 rListHosp.append(h)
 
-    return {'hospedagens':rListHosp}
+    return {'hospedagens': rListHosp}
 
 
-
-async def handleData(data, sock):
+async def handleData(data, sock,hosps):
     print(f"Recive data {data}")
     try:
         if data['action'] == 'hospedar':
-            sock.send(getHosps(hosps, data['diaIda'],data['cidade'], data['numeroDePessoas']))
+            sock.send(getHosps(hosps, data['diaIda'], data['cidade'], data['numeroDePessoas']))
         if data['action'] == 'reservar':
             sock.send(data)
     except Exception as ex:
         print(ex)
         sock.send(False)
+
 
 async def main():
     sock = EasySocketServer(5050)
@@ -32,8 +35,7 @@ async def main():
         hosps = json.load(json_file)
     while True:
         data = await sock.listen()
-        asyncio.run(handleData(data, sock))
-        
+        await handleData(data, sock,hosps)
 
 
 asyncio.run(main())
